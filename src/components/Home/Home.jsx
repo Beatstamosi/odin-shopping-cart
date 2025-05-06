beda1110422d5523c2e2d0b239002d254c8c7aca
Beatstamosi <moritz.lued@gmail.com>
2025-05-06 09:57:39 -0600

    Add Error Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import styles from "./home.module.css";
 import { Link } from "react-router-dom";
 
 function Home() {
-    return(
-        <div className={styles.containerHome}>
-            <h2>Welcome to Fake Store</h2>
-            <button><Link to="products">See All Products</Link></button>
-        </div>
-    )
+  return (
+    <div className={styles.containerHome}>
+      <h2>Welcome to Fake Store</h2>
+      <button>
+        <Link to="products">See All Products</Link>
+      </button>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,17 +1,19 @@
 import App from "../App.jsx";
 import Home from "./Home/Home.jsx";
+import ErrorPage from "./ErrorPage/ErrorPage.jsx";
 
 const routes = [
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                index: true,
-                element: <Home />
-            }
-        ]
-    }
-]
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+];
 
 export default routes;
