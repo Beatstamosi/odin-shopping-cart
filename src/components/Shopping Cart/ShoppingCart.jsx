b93175f5b4de39c1d0feb18e245967ee6f13f0d3
Beatstamosi <moritz.lued@gmail.com>
2025-05-07 17:01:12 -0600

    Add delete item from cart logic

--- src/components/Shopping Cart/ShoppingCart.jsx
+++ src/components/Shopping Cart/ShoppingCart.jsx
@@ -1,5 +1,6 @@
 import { useCart } from "../CartContext";
 import styles from "./ShoppingCart.module.css";
+import { Link } from "react-router-dom";
 
 function ShoppingCart() {
   const { shoppingCart, setShoppingCart } = useCart();
@@ -10,38 +11,64 @@ function ShoppingCart() {
   );
 
   // New logic for increase, decrease --> directly change amount in shopping Cart
-  // Logic for delete from Cart
-  
+
+  const deleteItem = (product) => {
+    const updatedCart = [...shoppingCart];
+    const index = updatedCart.findIndex(
+      (item) => item.product.id == product.id
+    );
+    updatedCart.splice(index, 1);
+    setShoppingCart(updatedCart);
+  };
 
   return (
-    <div className={styles.containerCart}>
-      <div>
-        <h2>Your Cart</h2>
-        <hr />
-        {shoppingCart.map((item) => (
-          <div className={styles.containerProducts}>
-            <button className={styles.deleteBtn}>x</button>
-            <div className={styles.containerProduct}>
-              <img src={item.product.image} alt="" />
-              <p>{item.product.title}</p>
-            </div>
-            <div className={styles.productPricing}>
-              <p className={styles.price}>{(item.product.price * item.quantity).toFixed(2)}€</p>
-              <div className={styles.quantityHandler}>
-                <button>-</button>
-                <input type="text" value={item.quantity} readOnly />
-                <button>+</button>
-              </div>
-            </div>
-          </div>
-        ))}
+    <>
+      <div className={styles.backToProducts}>
+        <Link to="/products">
+          <span> ← Back to all Products</span>
+        </Link>
       </div>
-      <div className={styles.gridRight}>
-        <p className={styles.total}>Total</p>
-        <span className={styles.totalAmount}>{total}€</span>
-        <button className={styles.checkoutBtn}>Checkout</button>
+      <div className={styles.containerCart}>
+        {shoppingCart.length > 0 && (
+          <>
+            <div>
+              <h2>Your Cart</h2>
+              <hr />
+              {shoppingCart.map((item) => (
+                <div className={styles.containerProducts} key={item.product.id}>
+                  <button
+                    className={styles.deleteBtn}
+                    onClick={() => deleteItem(item.product)}
+                  >
+                    x
+                  </button>
+                  <div className={styles.containerProduct}>
+                    <img src={item.product.image} alt="" />
+                    <p>{item.product.title}</p>
+                  </div>
+                  <div className={styles.productPricing}>
+                    <p className={styles.price}>
+                      {(item.product.price * item.quantity).toFixed(2)}€
+                    </p>
+                    <div className={styles.quantityHandler}>
+                      <button>-</button>
+                      <input type="text" value={item.quantity} readOnly />
+                      <button>+</button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className={styles.gridRight}>
+              <p className={styles.total}>Total</p>
+              <span className={styles.totalAmount}>{total}€</span>
+              <button className={styles.checkoutBtn}>Checkout</button>
+            </div>
+          </>
+        )}
+        {shoppingCart.length < 1 && <p>Your Cart is empty.</p>}
       </div>
-    </div>
+    </>
   );
 }
 
