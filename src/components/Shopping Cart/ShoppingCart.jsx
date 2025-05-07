b167ebf1edab559130c206ade93311360f8f3513
Beatstamosi <moritz.lued@gmail.com>
2025-05-07 16:49:46 -0600

    Styling Shopping Cart Page

--- src/components/Shopping Cart/ShoppingCart.jsx
+++ src/components/Shopping Cart/ShoppingCart.jsx
@@ -4,10 +4,14 @@ import styles from "./ShoppingCart.module.css";
 function ShoppingCart() {
   const { shoppingCart, setShoppingCart } = useCart();
 
-  const total = shoppingCart.reduce((sum, current) => sum + (current.product.price * current.quantity), 0);
+  const total = shoppingCart.reduce(
+    (sum, current) => sum + current.product.price * current.quantity,
+    0
+  );
 
   // New logic for increase, decrease --> directly change amount in shopping Cart
   // Logic for delete from Cart
+  
 
   return (
     <div className={styles.containerCart}>
@@ -15,26 +19,27 @@ function ShoppingCart() {
         <h2>Your Cart</h2>
         <hr />
         {shoppingCart.map((item) => (
-          <div>
-            <div>
+          <div className={styles.containerProducts}>
+            <button className={styles.deleteBtn}>x</button>
+            <div className={styles.containerProduct}>
               <img src={item.product.image} alt="" />
               <p>{item.product.title}</p>
             </div>
-            <div>
-              <p>{item.product.price * item.quantity}€</p>
+            <div className={styles.productPricing}>
+              <p className={styles.price}>{(item.product.price * item.quantity).toFixed(2)}€</p>
               <div className={styles.quantityHandler}>
                 <button>-</button>
                 <input type="text" value={item.quantity} readOnly />
                 <button>+</button>
               </div>
-              <button>x</button>
             </div>
           </div>
         ))}
       </div>
-      <div>
-        <p>Total</p>
-        <span>{total}€</span>
+      <div className={styles.gridRight}>
+        <p className={styles.total}>Total</p>
+        <span className={styles.totalAmount}>{total}€</span>
+        <button className={styles.checkoutBtn}>Checkout</button>
       </div>
     </div>
   );
