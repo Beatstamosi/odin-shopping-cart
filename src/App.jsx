aa66b3340d2416638901d1f2e390c47578406244
Beatstamosi <moritz.lued@gmail.com>
2025-05-08 07:29:17 -0600

    Add addToCart Notification

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,42 @@ import styles from "./App.module.css";
 import { ProductsContext } from "./components/productsContext.js";
 import ScrollTop from "./components/ScrollToTop.jsx";
 import { CartContext } from "./components/CartContext.js";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import ItemAdded from "./components/Item Added Notification/ItemAdded.jsx";
 
 function App() {
   const products = useLoaderData();
 
   const [shoppingCart, setShoppingCart] = useState([]);
 
+  // Added to Cart Notification
+  const cartRef = useRef(shoppingCart);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    if (shoppingCart.length > cartRef.current.length) {
+      setShowNotification(true);
+      cartRef.current = shoppingCart;
+
+      setTimeout(() => {
+        setShowNotification(false);
+      }, 1700);
+    }
+  }, [shoppingCart]);
+
   return (
     <CartContext.Provider value={{ shoppingCart, setShoppingCart }}>
-    < ProductsContext.Provider value={products}>
-      <ScrollTop />
-      <div className={styles.page}>
-        <Header />
-        <main className={styles.content}>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </ProductsContext.Provider>
+      <ProductsContext.Provider value={products}>
+        <ScrollTop />
+        <div className={styles.page}>
+          <Header />
+          <main className={styles.content}>
+            {showNotification && <ItemAdded />}
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </ProductsContext.Provider>
     </CartContext.Provider>
   );
 }
---
+++ src/components/Item Added Notification/ItemAdded.jsx
@@ -0,0 +1,11 @@
+import styles from "./ItemAdded.module.css"
+
+function ItemAdded() {
+    return (
+        <div className={styles.container}>
+            <p>Item Added to Cart!</p>
+        </div>
+    )
+}
+
+export default ItemAdded;
